0b02ed88c96cf16da784a70f98b24e45d9bde396
Dlzxn <aleshamarichev09@gmail.com>
2025-01-03 01:36:36 +0300

    v0.1 new request from API UNN

--- parser/unn_request.py
+++ parser/unn_request.py
@@ -1,43 +1,18 @@
-import aiohttp
-import asyncio
+import aiohttp, asyncio, time
 
+class UnnRequest:
+    def __init__(self, login, password):
+        self.login: str = login
+        self.psw: str = password
+        self.__new_format()
+        asyncio.run(self.get_ruz())
 
-async def login(session, url, username, password):
-    login_data = {
-        'AUTH_FORM': 'Y',
-        'TYPE': 'AUTH',
-        'backurl': '/ruz/main?_url=%2Fmain',
-        'USER_LOGIN': username,
-        'USER_PASSWORD': password,
-        'USER_REMEMBER': 'Y'  # Опционально, если нужно запомнить пользователя
-    }
+    def __new_format(self):
+        student_number: int = int(self.login[1:])
+        self.format: str = f'https://portal.unn.ru/ruzapi/schedule/student/{student_number-24073692}?start=2024.12.09&finish=2024.12.15&lng=1'
 
-    # Отправляем POST-запрос для входа
-    async with session.post(url, data=login_data) as response:
-        response.raise_for_status()  # Проверка на ошибки
-        return response.url  # Возвращаем URL для подтверждения входа
-
-
-async def access_protected_page(session, protected_url):
-    async with session.get(protected_url) as response:
-        response.raise_for_status()  # Проверка на ошибки
-        return await response.text()  # Получаем содержимое страницы
-
-
-async def main(login, password):
-    # Создаем сессию
-    async with aiohttp.ClientSession() as session:
-        login_url = 'https://portal.unn.ru/'  # Замените на реальный URL авторизации
-        protected_url = 'https://portal.unn.ru/ruz/main'  # Замените на реальный защищенный URL
-        username = login  # Ваше имя пользователя
-        password = password  # Ваш пароль
-
-        try:
-            await login(session, login_url, username, password)  # Сохраняем сессию
-            page_content = await access_protected_page(session,
-                                                       protected_url)  # Используем сессию для доступа к защищенной странице
-            return page_content  # Выводим содержимое защищенной страницы
-        except Exception as e:
-            return f"Произошла ошибка: {e}"
-
-
+    async def get_ruz(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.format) as response:
+                print(await response.json())
+                self.json_response = await response.json()
