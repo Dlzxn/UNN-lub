4da35ffd00f45abd0190f0d89fff975b51f39264
Dlzxn <aleshamarichev09@gmail.com>
2025-01-02 22:16:55 +0300

    fix

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
 from parser.unn_request import main
 from analytic.analyhtml import AnalyticHtml
-awfafafafawfafwafwafwafawfafwafafafafwawawafawfafawfawfafawfafawfafwafafawfawfaffawfafwafawfawfawfa
+
 cl = AnalyticHtml(sy)
